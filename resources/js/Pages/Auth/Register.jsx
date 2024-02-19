1a0b788bae8b6fe76fded52121fc7255c22b291a
kaykyls <20211bsifsa0025@ifba.edu.br>
2024-02-18 22:50:19 -0300

    fix ui bug

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -163,7 +163,7 @@ export default function Register() {
                         <button
                             disabled={processing}
                             type="submit"
-                            className="rounded-md bg-main-color px-3 py-2 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-main-color"
+                            className="rounded-md bg-main-color px-3 py-2 text-base font-semibold text-white shadow-sm hover:bg-main-color-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-main-color"
                             onClick={submit}
                         >
                         Registrar
