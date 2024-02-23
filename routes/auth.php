e91f14c2e34beb2386f40e8b391865544e84e806
kaykyls <20211bsifsa0025@ifba.edu.br>
2024-02-23 00:12:50 -0300

    update https

--- routes/auth.php
+++ routes/auth.php
@@ -16,7 +16,7 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->secure();;
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
